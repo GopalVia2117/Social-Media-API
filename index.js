30ab733271623513580fb8133e810d4c6a37bc7a
Gopal Vishwakarma <gopals2117@gmail.com>
2023-05-28 15:14:22 +0530

    Adding route to upload images at cloudinary cdn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -35,6 +37,20 @@ app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 app.use(morgan("common"));
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+cloudinary.config({
+  cloud_name: "djop9ubq6",
+  api_key: "612891234139542",
+  api_secret: "M6L4hKyr0nt9LoVXSSKcgSMHuj0",
+});
+
 // console.log(path.join(__dirname, "public/images"));
 // app.get("/images", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public/images/ad.png"));
@@ -50,22 +66,24 @@ app.get("/", (req, res) => {
   res.send("welcome to homepage..");
 });
 
-// multer configuration
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "public/images"));
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-const upload = multer({ storage });
-app.post("/api/upload", upload.single("file"), (req, res, next) => {
-  try {
-    return res.status(200).json("File uploaded successfully");
-  } catch (error) {
-    console.log(error);
+app.post("/api/upload", (req, res, next) => {
+  let sampleFile;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json("No files were uploaded.");
   }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.file;
+  const result = cloudinary.uploader.upload(req.files.file.tempFilePath);
+
+  result
+    .then((data) => {
+      return res.status(200).json({ url: data.secure_url });
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 });
 
 app.listen(PORT, () => {
